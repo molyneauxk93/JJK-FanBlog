68f66acfd365b7e6a5452232af8c7bae563ed58c
Josh Mol <joshua.molyneaux@live.com>
2024-07-30 00:10:41 -0400

    Adding props to load blog post titles into blog page

--- client/src/components/BlogPosts.js
+++ client/src/components/BlogPosts.js
@@ -6,7 +6,7 @@ const BlogPosts = (props) => {
     return (
         <div className="card w-75 text-center post-title">
             <div className="card-body">
-                <h2 className="card-title blog-title cut-text">Card title</h2>
+                <h2 className="card-title blog-title cut-text">{props.data.title}</h2>
                 <button className="btn btn-primary button">View Post</button>
             </div>
         </div>
--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -5,15 +5,33 @@ export default function Blog() {
     // Create object with blogposts to render 
     const blogposts = [
         {
-
-        }
+            title: 'Test title 1 is this a good test'
+        },
+        {
+            title: 'Test title 2 is this a good test'
+        },
+        {
+            title: 'Test title 3 is this a good test'
+        },
+        {
+            title: 'Test title 4 is this a good test'
+        },
+        {
+            title: 'Test title 5 is this a good test'
+        },
+        {
+            title: 'Test title 6 is this a good test'
+        },
+        {
+            title: 'Test title 7 is this a good test'
+        },
     ]
 
     // return blogpost object with data to be loaded into the photos page
     return (
         <div>
             {
-                blogposts.map((photos) => <BlogPosts data={photos} />)
+                blogposts.map((posttitles) => <BlogPosts data={posttitles} />)
             }
         </div>
     );
