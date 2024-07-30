12c5fcaae803dbfd9a3d0bd0aa662a89b71fe63c
Josh Mol <joshua.molyneaux@live.com>
2024-07-29 23:55:42 -0400

    Adding styling for blog page

--- client/src/components/BlogPosts.js
+++ client/src/components/BlogPosts.js
@@ -4,9 +4,11 @@ import React from 'react';
 const BlogPosts = (props) => {
 
     return (
-        <div class="card text-white bg-primary mb-3" style={{ maxwidth:'18rem', margin: '30px' }}>
-            {/* will make this link to full blog post with comments */}
-            <div class="card-header" style={{ textAlign: 'center'}}>Blog Post Title</div>
+        <div className="card w-75 text-center post-title">
+            <div className="card-body">
+                <h2 className="card-title blog-title cut-text">Card title</h2>
+                <button className="btn btn-primary button">View Post</button>
+            </div>
         </div>
     );
 
