79e26ff17eb9f5ddf57ca775a99f9cd88bf15ca1
Josh Mol <joshua.molyneaux@live.com>
2024-08-04 00:02:14 -0400

    Adding some of the add post functionality

---
+++ client/src/pages/NewPost.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_POST } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+
+const AddPost = () => {
+
+    const [formState, setFormState] = useState({ title: '', description: '', postAuthor: ''});
+    const [addpost, { error }] = useMutation(ADD_POST);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+            const mutationResponse = await addpost({
+                variables: {
+                    title: formState.title,
+                    description: formState.description,
+                    postAuthor: formState.postAuthor,
+                },
+            });
+            window.location.reload(false);
+    }
+    
+        const handleChange = (event) => {
+            const { name, value } = event.target;
+            setFormState({
+                ...formState,
+                [name]: value,
+            });
+        };
+    
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default AddPost;
