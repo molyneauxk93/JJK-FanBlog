44bddfd374dd9d57341b9ae704149541f4fbbae6
Josh Mol <joshua.molyneaux@live.com>
2024-08-09 23:58:19 -0400

    Adding updates to styling of navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -23,11 +23,11 @@ const Navbar = () => {
 
                 {isLoggedIn
                     ? (
-                        <div>
-                            <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div className="dropdown-center">
+                            <button type="button" className="btn menu-button dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                 Menu
                             </button>
-                            <ul className="dropdown-menu" style={{ position: "relative" }}>
+                            <ul className="dropdown-menu">
                                 <Link to="/newpost"><li className="dropdown-item">New Post</li></Link>
                                 <Link to="/" onClick={handleLogout}><li className="dropdown-item">Logout</li></Link>
                             </ul>
