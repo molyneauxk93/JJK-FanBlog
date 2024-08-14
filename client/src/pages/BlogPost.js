24a3b22ee88ea50ed5cfc53a2ea784a7f1ee773b
Josh Mol <joshua.molyneaux@live.com>
2024-08-13 23:47:56 -0400

    adding working notes for future changes to blogpost page

--- client/src/pages/BlogPost.js
+++ client/src/pages/BlogPost.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
-import { useRef } from 'react';
+import React from 'react';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
 
+// Use params to get the id of a single blog post 
+// then will take the id and run a use query to find single blogpost 
+// from there will layout how I want a single blog post page to look below 
+// on click from blog page and profile page - link to this page where you can see
+// full blogpost and comments if any
 
 const BlogPost = () => {
     
