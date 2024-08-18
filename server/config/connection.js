c6b02182533347386ccd69cad14fb025c370bcf6
Josh Mol <joshua.molyneaux@live.com>
2024-08-17 23:38:51 -0400

    ...

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jjk-blog-app');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jjkfanblog');
 
 module.exports = mongoose.connection;
